47cd6f581be2fbe5583cebff859b5882c8a3344a
Your name here <your_email_address@example.com>
2020-12-12 14:42:19 +0200

    change bubbles to snowflakes hehe

--- bubbles-demo/index.js
+++ bubbles-demo/index.js
@@ -1,8 +1,9 @@
 const FPS = 60;
 const PADDING_X = 40;
-const BUBLES_COLORS = ['yellow','green','red','blue', 'pink', 'soap'];
+const BUBLES_COLORS = ['yellow','green','red','blue', 'soap'];
 const canvas = document.createElement('canvas');
 const gameBox = document.getElementById('game-box');
+let discount = 0;
 let isPaused = false;
 gameBox.append(canvas);
 const scaleCanvas = function() {
@@ -21,6 +22,7 @@ const mathRandom = function(min, max) {
 
 const bubblesArray = new Array();
 const effectsArray = new Array();
+const textArray = new Array();
 const images = new Object();
 
 const loadImage = function (path) {
@@ -42,7 +44,7 @@ class Bubble {
         this.size = object.size ? (object.size/100) * canvas.width : mathRandom(0.1, 0.15) * canvas.width;
         this.imageName = object.imageName ? object.imageName : BUBLES_COLORS[Math.floor(Math.random()*BUBLES_COLORS.length)];
         this.x = mathRandom(0 + PADDING_X + this.size, canvas.width - PADDING_X - this.size);
-        this.y = canvas.height + this.size;
+        this.y = 0 - this.size;
         this.speed = object.speed ? object.speed : mathRandom(0.5, 2);
         this.deformation = {
             y: 0,
@@ -52,45 +54,116 @@ class Bubble {
         }
     }
     destroy = function() {
-        effectsArray.push(new Effect(this.imageName, this.size, this.x, this.y));
+        discount = (discount*10+1)/10;
+        effectsArray.push(new Effect(this.size, this.x, this.y));
+        textArray.push(new Text('+0.1%', this.x+(this.size/2), this.y+(this.size/2)));
     }
 }
 class Effect {
-    constructor(color, size, x, y) {
+    constructor(size, x, y) {
         this.x = x;
         this.y = y;
-        this.color = color;
         this.size = size / 4;
         this.entities = new Array();
         this.centerX = x + (this.size/2);
         this.centerY = y + (this.size/2);
         this.count = 7;
-        angle = Math.random()*Math.PI*2;
+        this.entities = [];
+        this.opacity = 0;
         for (let i = 0; i < this.count; i++) {
-            const element = array[i];
+            let q = 1
+            if (i < this.count/2) {
+                q = -1
+            }
+            this.entities.push({
+                x: this.centerX - mathRandom(0.1, size/4)*q,
+                y: this.centerY - mathRandom(0.1, size/4)*q,
+                moveRight: (i % 2 == 0),
+                speed: mathRandom(0.3, 3)
+            });
             
         }
         console.log(this);
     }
 }
-
+class Text {
+    constructor(text, x, y) {
+        this.size = 1;
+        this.text = text;
+        this.x = x;
+        this.y = y;
+        console.log(this);
+    }
+}
 const render = function() {
     ctx.fillStyle = "#222222";
     ctx.fillRect(0,0,canvas.width, canvas.height);
     bubblesArray.forEach(el => {
         ctx.drawImage(images[el.imageName], el.x - el.deformation.x, el.y - el.deformation.y, el.size + el.deformation.x, el.size + el.deformation.y);
-        // console.log(el.y);
+        
     });
-    // effectsArray.forEach(effect => {
-    //     effect.entities.forEach(el => {
-    //         ctx.drawImage(images[effect.color], el.xC * effect.size + effect.centerX, el.yC * effect.size + effect.centerY, effect.size, effect.size);
-
-    //     });
-    // });
-
-
-
-
+    effectsArray.forEach(effect =>{
+    effect.entities.forEach(entity => {
+        // console.log(entity);
+        ctx.drawImage(images.snow_particle, entity.x, entity.y, effect.size, effect.size); 
+    });
+    });
+    bubblesArray.forEach((bubble, i) => {
+        bubble.y += bubble.speed * 60/FPS;
+        const deformation = bubble.deformation;
+        if (deformation.xIsGrowing) {
+            deformation.x+=0.10;
+        }
+        else {
+            deformation.x-=0.10;
+        }
+        if (deformation.yIsGrowing) {
+            deformation.y+=0.10;
+        }
+        else {
+            deformation.y-=0.10;
+        }
+        if (deformation.x > 6 || deformation.x < 0) {
+            deformation.xIsGrowing = !deformation.xIsGrowing;
+        }
+        if (deformation.y > 6 || deformation.y < 0) {
+            deformation.yIsGrowing = !deformation.yIsGrowing;
+        }
+        if (bubble.y > canvas.height + bubble.size) bubblesArray.splice(i, 1);
+        if (bubble.destroyed) {
+            bubblesArray.splice(i, 1);
+        }
+    });
+    effectsArray.forEach((effect, i) => {
+        effect.size -= 0.07;
+        if (effectsArray.length > 3) {
+            effect.size -= 0.3;
+        }
+        effect.entities.forEach(entity => {
+            const q = entity.moveRight ? 1 : -1
+            entity.x += entity.speed * q;
+            entity.y += Math.pow(entity.speed,2);
+        });
+        if (effect.size <= 0) {
+            effectsArray.splice(i, 1);
+        }
+    });
+    textArray.forEach((text, i) => {
+        ctx.font = `bold ${40*text.size}px Game-font`;
+        text.size-=0.01;
+        ctx.fillStyle = "#ffffff";
+        ctx.fillText(text.text, text.x, text.y);
+        ctx.strokeText(text.text, text.x, text.y);
+        text.y-=0.5;
+        if (text.size <= 0) {
+            textArray.splice(i, 1);
+        }
+    });
+    ctx.fillStyle = "#222222";
+    ctx.fillRect(0,0,canvas.width, 50);
+    ctx.fillStyle = "#ffffff";
+    ctx.font = 'bold 40px Game-font';
+    ctx.fillText('Score ' + discount + '%', canvas.width/2,40);
 }
 
 
@@ -108,40 +181,60 @@ window.main = function () {
 //loading imgs
 
 const start = async function() {
+    ctx.fillStyle = "#222222";
+    ctx.fillRect(0,0,canvas.width, canvas.height);
+    ctx.font = 'bold 40px Game-font';
+    ctx.fillStyle = "#ffffff";
+    ctx.textAlign = 'center';
+    ctx.fillText('Loading...', canvas.width/2, canvas.height/2);
+    ctx.strokeStyle = "#536DFE";
+    ctx.strokeText('Loading...', canvas.width/2, canvas.height/2);
     for (const color of BUBLES_COLORS) {
-        const img = await loadImage(`./img/${color}_bubble.png`);
+        const img = await loadImage(`./img/${color}_snow.png`);
         images[color] = img;
     }
+    images.snow_particle = await loadImage(`./img/snow_particle.png`)
     console.log(images);
     bubblesArray.push(new Bubble({size: 10, imageName: 'yellow'}));
-    setInterval(()=> {
-        bubblesArray.forEach((bubble, i) => {
-            bubble.y -= bubble.speed * 60/FPS;
-            const deformation = bubble.deformation;
-            if (deformation.xIsGrowing) {
-                deformation.x+=0.10;
-            }
-            else {
-                deformation.x-=0.10;
-            }
-            if (deformation.yIsGrowing) {
-                deformation.y+=0.10;
-            }
-            else {
-                deformation.y-=0.10;
-            }
-            if (deformation.x > 6 || deformation.x < 0) {
-                deformation.xIsGrowing = !deformation.xIsGrowing;
-            }
-            if (deformation.y > 6 || deformation.y < 0) {
-                deformation.yIsGrowing = !deformation.yIsGrowing;
-            }
-            if (bubble.y < 0 - bubble.size) bubblesArray.splice(i, 1);
-            if (bubble.destroyed) {
-                bubblesArray.splice(i, 1);
-            }
-        });
-    }, 1000/FPS);
+    // setInterval(()=> {
+        // bubblesArray.forEach((bubble, i) => {
+        //     bubble.y += bubble.speed * 60/FPS;
+        //     const deformation = bubble.deformation;
+        //     if (deformation.xIsGrowing) {
+        //         deformation.x+=0.10;
+        //     }
+        //     else {
+        //         deformation.x-=0.10;
+        //     }
+        //     if (deformation.yIsGrowing) {
+        //         deformation.y+=0.10;
+        //     }
+        //     else {
+        //         deformation.y-=0.10;
+        //     }
+        //     if (deformation.x > 6 || deformation.x < 0) {
+        //         deformation.xIsGrowing = !deformation.xIsGrowing;
+        //     }
+        //     if (deformation.y > 6 || deformation.y < 0) {
+        //         deformation.yIsGrowing = !deformation.yIsGrowing;
+        //     }
+        //     if (bubble.y > canvas.height + bubble.size) bubblesArray.splice(i, 1);
+        //     if (bubble.destroyed) {
+        //         bubblesArray.splice(i, 1);
+        //     }
+        // });
+        // effectsArray.forEach((effect, i) => {
+        //     effect.size -= 0.07;
+        //     effect.entities.forEach(entity => {
+        //         const q = entity.moveRight ? 1 : -1
+        //         entity.x += entity.speed * q;
+        //         entity.y += Math.pow(entity.speed,2);
+        //     });
+        //     if (effect.size <= 0) {
+        //         effectsArray.splice(i, 1);
+        //     }
+        // });
+    // }, 1000/FPS);
     setInterval(()=> {
         if(!Math.floor(mathRandom(0, 3))) {
             bubblesArray.push(new Bubble({}));
@@ -175,5 +268,3 @@ canvas.addEventListener('click', (e) => {
         }
     });
 });
-
-new Effect();
