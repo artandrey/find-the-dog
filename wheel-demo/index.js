4d15d03b1cd372a43e6d331b2e59bdde09e9de2e
Your name here <your_email_address@example.com>
2020-12-13 18:59:25 +0200

    change ip

--- wheel-demo/index.js
+++ wheel-demo/index.js
@@ -1,5 +1,5 @@
 let angle = 90;
-const BACKEND_URL = 'http://176.37.194.15:8080';
+const BACKEND_URL = 'http://127.0.0.1:8080';
 [...document.querySelectorAll('.moving-wheel-text-box')].forEach(el => {
     if (angle > 100 && angle < 230) {
         const span = el.querySelector('span')
@@ -9,6 +9,15 @@ const BACKEND_URL = 'http://176.37.194.15:8080';
     el.style.transform = `translateX(-50%) translateY(-50%) rotate(${angle}deg)`;
     angle += 45;
 });
+
+const wrapper = document.getElementById('wheel-wrapper');
+const showModal = function() {
+    wrapper.style.display = "block";
+}
+const closeModal = function() {
+    wrapper.style.display = "none";
+}
+document.getElementById('wheel-close-btn').addEventListener('click', closeModal);
 
 const form = document.getElementById('phone-form');
 const textInput = document.getElementById('phone-input');
@@ -29,28 +38,39 @@ form.addEventListener('submit' ,event => {
 
     });
     console.log("SUBMIT");
-        const data = {}
-        data.optionIndex = Math.floor(Math.random()*8);
-    
+    // fetch('http://127.0.0.1:8080/wheel', {method: 'POST', body: jsonString}).then(data => data.json())
+    // .then(data => {
         if (data.error === 'phone') {
             alert('Этот номер телефона уже использовали');
+            return;
         } 
-        if (data.optionIndex) {
+        if (!Number.isNaN(data.optionIndex)) {
             let angle = 0;
             let speed = 2;
-            const optionAngle = 360/8*data.optionIndex+90
+            let optionAngle = 360/8*data.optionIndex+90
+            optionAngle = optionAngle > 360 ? optionAngle - 360 : optionAngle;
             submitBtn.setAttribute('disabled', '');
             const animationInterval = setInterval(()=> {
                 angle+=speed;
                 if (speed < 0.5 && angle <= 360) {
-                    if (angle >= 360) {
-                        angle = 1;
-                    }
                     if(angle > optionAngle - 10) {
                         speed -=0.01;
                     }
                     if(angle > optionAngle) {
                         clearInterval(animationInterval);
+                        setTimeout(() => {
+                            document.querySelector('.wheel-container').style.display = 'none';
+                            document.querySelector('.wheel-info-box').style.display = 'none';
+                            const container = document.createElement('div');
+                            container.classList.add('modal-end-container');
+                            container.innerHTML = `
+                            <h3 class="wheel-modal-end-h3">${data.optionName}</h3>
+                            <h4 class="wheel-modal-end-h4">Поздравляем! Не пропустите звонок, мы скоро свяжемся с Вами!</h4>
+                            `
+                            const modal = document.querySelector('.wheel-modal');
+                            modal.append(container);
+                            modal.classList.add('end-modal-wheel-color');
+                        },1000)
                     }
                 }
                 else {
@@ -62,6 +82,8 @@ form.addEventListener('submit' ,event => {
                 wheel.style.transform = `rotate(${angle}deg)`;
             }, 10);
         }
-    
+    // });
 
 });
+
+showModal();
